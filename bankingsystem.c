334de638b86be77004b74e466824251005ccbece
Omwamii <ianalpha@protonmail.com>
2023-02-07 16:48:07 +0300

    created createaccount fn

--- bankingsystem.c
+++ bankingsystem.c
@@ -2,20 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <conio.h>
 
 //structure for data of user
 typedef struct pass {
-    char username[50];
+    char *username;
     int date, month, year;
-    char idnum[10];
-    char adharnum[20];
-    char fname[20];
-    char lname[20];
-    char fathname[20];
-    char mothname[20];
-    char address[50];
-    char accounttype[20];
-}pass;
+    int idnum;
+    char *fname;
+    char *lname;
+    char *address;
+    char *accounttype;
+    char password[20];
+}user;
 
 // Structure to keep track of money transfer
 typedef struct money {
@@ -24,6 +23,67 @@ typedef struct money {
     long int money1;
 }money;
 
-typedef struct userpass {
-    char password[50];
-}userpass;
+void createaccount(void)
+{
+    char password[20], ch;
+    int i;
+    FILE *fp;
+    user u1;
+
+    fp = fopen("accounts.bin", "ab");
+
+    system("cls");
+
+    printf("********** CREATE ACCOUNT **********\n");
+    printf("Enter first name:");
+    scanf("%s", &u1.fname);
+    printf("Enter last name:");
+    scanf("%s", &u1.lname);
+    printf("Enter a username:");
+    scanf("%s", &u1.username);
+    printf("Enter date of birth:");
+    scanf("%d", &u1.date);
+    printf("Enter month of birth:");
+    scanf("%d", &u1.month);
+    printf("Enter year of birth:");
+    scanf("%d", &u1.year);
+    printf("Enter account type");
+    scanf("%s", &u1.accounttype);
+    printf("Enter your town name");
+    scanf("%s", &u1.address);
+    printf("Enter your id number:");
+    scanf("%d", &u1.idnum);
+    printf("Enter your password (8 characters minimum)");
+
+    for (i = 0; i <= 20; i++)
+    {
+        ch = getch();
+        if (ch != 10) //10 is ascii for ENTER key
+        {
+            password[i] = ch;
+            ch = '*'; //to hide password as ***
+            printf("%c", ch);
+        }
+        else
+        {
+            if (strlen(password) < 8)
+            {
+                printf("Password is too short!");
+                continue;
+            }
+
+            break;
+        }
+    }
+
+    if (fp == NULL)
+    {
+        printf("Error creating file");
+        exit(1);
+    }
+
+    fwrite(&u1, sizeof(user), 1, fp);
+
+    fclose(fp);
+
+}
